37b10e1e0182b299aeef13e614a19fcdf22884a3
Ruth Carter <recarter07@gmail.com>
2016-11-07 20:04:41 -0600

    Javagram save overwrite work

--- java-track/assignments/javagram/Javagram.java
+++ java-track/assignments/javagram/Javagram.java
@@ -40,7 +40,7 @@ public class Javagram {
 			}
 			if (choice == 4) {
 				System.out.println("Güten Tog");
-				return new GrayScaleFilter();
+				return new NoFilter();
 			}
 			else {
 			// re-prompt? - needed?
@@ -65,7 +65,7 @@ public class Javagram {
 		System.out.println("1 = BlueFilter");
 		System.out.println("2 = GreenFilter");
 		System.out.println("3 = RedFilter");
-		System.out.println("4 = GrayScaleFilter");
+		System.out.println("4 = NoFilter");
 		
 //		getFilter(); - redundant
 		
@@ -138,34 +138,75 @@ public class Javagram {
 		
 		System.out.println("Image successfully filtered");
 		
+		
 		// SAVING IMAGE...
 		// save image, if desired
 		
 		System.out.println("Save image to (relative to " + dir + ") (type 'exit' to quit w/o saving):");
 		String fileName = in.next();
+//		String overwrite = in.next();        // this must be INSIDE the if .equals statement below to be waited for and considered by comp
 		
-		// TODO - if the user enters the same file name as the input file, confirm that they want to overwrite the original
+		// TODO - if the user enters the same file name as the input file,
+		//    confirm that they want to overwrite the original
 		
+//		if (fileName.equals("exit")) {           // for some reason, input needs to include "" to be accepted in program run up here...
+//			System.out.println("Image not saved");
+//		}
 //		if (fileName.equals(current fileName)) { prompt confirmation of overwrite }
 		
 //		for saving without overwriting...
 //		if (filename.equals("exit)) { x = "y";}...
+//		else if (fileName.equals(relPath)) {
+		if (fileName.equals(relPath)) {
+//			System.out.println("Are you sure you want to overwrite original image? Type y or n.");  // there's no pause for input after this Q appears when there's no overwrite var introed afterwards...
+			System.out.println("Are you sure you want to overwrite original image? Type 1 for yes or 2 for no.");
+//			String overwrite = in.next();   // using String here is confused with the fileName (also an input String)
+			int overwrite = in.nextInt();   // using int instead of String differentiates overwrite from fileName
+//			if (overwrite == "y") {
+			if (overwrite == 1) {
+				System.out.println("Did you hear me? It was a yes.");
+				String absFileName = dir + File.separator + fileName;
+				processed.save(absFileName);
+				System.out.println("Image saved to " + absFileName);
+			} else if (overwrite == 2) {
+//			} else if (overwrite == "n") {
+				System.out.println("Image not saved");
+			} else {                                          // if overwrite input is a String, InputMismatchException thrown
+				System.out.println("You don't understand?");  // (op) I want it to try again...maybe put all this in a method and call the method again??
+				System.out.println("Image not saved");
+			}
+		} 
+
+//		System.out.println("Save image to (relative to " + dir + ") (type 'exit' to quit w/o saving):");
+//		String fileName = in.next();
 		
 		
+//		THIS IF IS VITAL FOR THE ABOVE COMMAND SUGGESTING "exit" to be able to follow through
 		if (fileName.equals("exit")) {
 			System.out.println("Image not saved");
-		} else {
-			String absFileName = dir + File.separator + fileName;
-//			if (absFileName == existing file name) ask to confirm overwrite
-			processed.save(absFileName);
-			System.out.println("Image saved to " + absFileName);
-		}	
+		}
+//		GIVEN CODE - IF YOU IGNORE THE OVERWRITE STATEMENTS, THE COMMENTED CODE BELOW WILL AUTOMATICALLY SAVE THE JPG
+//		else {
+//			String absFileName = dir + File.separator + fileName;
+//		
+////			if (absFileName == existing file name) ask to confirm overwrite
+//			processed.save(absFileName);
+//			System.out.println("Image saved to " + absFileName);
+//		}	
 		
 		// close input scanner
+		System.out.println("We end here.");
 		in.close();
-	}
+		System.out.println("Now what? It's still running...");  // the program doesn't stop until I "quit" from the Javagram file
+//		System.out.println(in.next());       // this gives a "Scanner closed" illegal state exception error
+	}            // this is the curly attached to the beginning of main...
 	
 }
+
+
+
+
+
 
 
 
--- java-track/assignments/javagram/filters/NoFilter.java
+++ java-track/assignments/javagram/filters/NoFilter.java
@@ -9,6 +9,25 @@ public class NoFilter implements Filter {
 		
 		Picture processed = new Picture(original.width(), original.height());
         
+	    //get each pixel one by one
+	    for (int i = 0; i < original.width(); i++) {
+	      for (int j = 0; j < original.height(); j++) {
+	    	  
+	    	  Color c = original.get(i, j);
+	          
+	          //get color components from each pixel
+	          int r = c.getRed();
+	          int g = c.getGreen();
+	          int b = c.getBlue();
+	          
+//	          int newBlue = (r + g + b) / 3;
+	          
+	          processed.set(i, j, new Color(r, g, b));
+	    	  
+	      }
+	    }
+		
+//	    CODE COMMENTED OUT BELOW = THE IMAGE REPROCESSED IS ALL BLACK
 //	    //get each pixel one by one
 //	    for (int i = 0; i < original.width(); i++) {
 //	      for (int j = 0; j < original.height(); j++) {
